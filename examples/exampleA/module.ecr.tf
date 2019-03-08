f7a2590d46768751e16a0bc18c341e3ab2854b25
James Woolfenden <James.Woolfenden@slalom.com>
2019-03-08 10:01:48 +0000

    cleanup

--- examples/exampleA/module.ecr.tf
+++ examples/exampleA/module.ecr.tf
@@ -1,6 +1,5 @@
 module "ecr" {
-  source           = "github.com/JamesWoolfenden/terraform-aws-ecr"
-  version          = "0.0.4"
+  source           = "../../"
   name             = "${var.name}"
   region           = "${var.region}"
   repositorypolicy = "${data.aws_iam_policy_document.allowlocals.json}"
